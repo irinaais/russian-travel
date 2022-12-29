f9ad36f4214645243d94c7eafaf4b61de75aef73
Iris <irinaais90@yandex.ru>
2022-12-30 01:33:40 +0400

    feat: add en lang

--- lang.js
+++ lang.js
@@ -11,6 +11,10 @@ const langArr = {
     "ru": "Ирина Осипова",
     "en": "Irina Osipova",
   },
+  "header__logo": {
+    "ru": "Карта России",
+    "en": "Map of Russia",
+  },
   "lead__title": {
     "ru": "Путешествия по&nbsp;России",
     "en": "Traveling in&nbsp;Russia",
@@ -18,5 +22,219 @@ const langArr = {
   "lead__subtitle": {
     "ru": "Настоящая страна не в выпусках новостей, а здесь.",
     "en": "The real country is not in the news, but here.",
+  },
+  "lead__image": {
+    "ru": "девушка спит на нижней полке у окна в плацкартном вагоне поезда",
+    "en": "the girl sleeps on a shelf near the window in the train",
+  },
+  "lead__caption": {
+    "ru": "ваша полка — верхняя",
+    "en": "YOUR SHELF IS TOP",
+  },
+  "intro__title": {
+    "ru": "Чего мы там не&nbsp;видели?",
+    "en": "What we didn't see there",
+  },
+  "intro__text": {
+    "ru": "По опросам ВЦИОМ, 95% россиян мечтают куда-нибудь поехать, но только 36% планируют провести отпуск в родной стране. Мол, чего мы \n" +
+      "            тут, дома, не видели? На самом деле, Россия — это целая вселенная с ласковым морем юга, густыми лесами Саян и суровыми льдами плато Путорана. \n" +
+      "            А ещё увидеть все эти красоты можно без миллионов на счету, загранпаспорта и многочасовых перелетов. Как, например, Вера Башмакова — смелая молодая \n" +
+      "            мама, которая взяла в охапку троих детей, усадила их в свою «Ладу» и проехала 20 тысяч километров по родной стране. Мы выбрали и описали некоторые \n" +
+      "            интересные места, достойные вашего отпуска.",
+    "en": "According to VTsIOM polls, 95% of Russians dream of going somewhere, but only 36% plan to spend their holidays in their native country. Like, what have we not seen " +
+      "here at home? In fact, Russia is a whole universe with the gentle sea of ​​the south, the dense forests of the Sayans and the harsh ice of the Putorana Plateau. " +
+      "And you can also see all these beauties without millions in your account, a passport and many hours of flights. Like, for example, Vera Bashmakova is a brave young\n" +
+      "mother, who took three children in her arms, put them in her Lada and drove 20 thousand kilometers in her native country. We have selected and described some interesting places " +
+      "worthy of your vacation.",
+  },
+  "intro__list-item11": {
+    "ru": "Часовых поясов <span class=\"intro__list-num\">11</span>",
+    "en": "Time Zones <span class=\"intro__list-num\">11</span>",
+  },
+  "intro__list-item12": {
+    "ru": "Объектов природного наследия ЮНЕСКО <span class=\"intro__list-num\">12</span>",
+    "en": "UNESCO natural heritage sites <span class=\"intro__list-num\">12</span>",
+  },
+  "intro__list-item16": {
+    "ru": "Объектов культурного наследия ЮНЕСКО <span class=\"intro__list-num\">16</span>",
+    "en": "UNESCO cultural heritage sites <span class=\"intro__list-num\">16</span>",
+  },
+  "intro__list-item105": {
+    "ru": "Природных заповедников <span class=\"intro__list-num\">105</span>",
+    "en": "Nature reserves <span class=\"intro__list-num\">105</span>",
+  },
+  "intro__list-item241": {
+    "ru": "Аэропортов <span class=\"intro__list-num\">241</span>",
+    "en": "Airports <span class=\"intro__list-num\">241</span>",
+  },
+  "photo-grid__item1": {
+    "ru": "окно поезда",
+    "en": "train window",
+  },
+  "photo-grid__item2": {
+    "ru": "горы",
+    "en": "mountains",
+  },
+  "photo-grid__item3": {
+    "ru": "туман",
+    "en": "fog",
+  },
+  "photo-grid__item4": {
+    "ru": "лес в тумане",
+    "en": "forest in the fog",
+  },
+  "photo-grid__item5": {
+    "ru": "лед",
+    "en": "ice",
+  },
+  "photo-grid__item6": {
+    "ru": "байкал",
+    "en": "Baikal",
+  },
+  "photo-grid__item7": {
+    "ru": "эльбрус",
+    "en": "Elbrus",
+  },
+  "photo-grid__item8": {
+    "ru": "дорожный знак пешеходный переход",
+    "en": "road sign pedestrian crossing",
+  },
+  "photo-grid__item9": {
+    "ru": "камчатка",
+    "en": "Kamchatka",
+  },
+  "photo-grid__item10": {
+    "ru": "горы камчатки",
+    "en": "mountains of Kamchatka",
+  },
+  "photo-grid__item11": {
+    "ru": "байкал пляж горы",
+    "en": "Baikal beach mountains",
+  },
+  "photo-grid__item12": {
+    "ru": "горы в тумане",
+    "en": "mountains in the fog",
+  },
+  "places__title1": {
+    "ru": "Куршская коса",
+    "en": "Curonian Spit",
+  },
+  "places__title2": {
+    "ru": "Кольский",
+    "en": "Kola Peninsula",
+  },
+  "places__title3": {
+    "ru": "Алтай",
+    "en": "Altai",
+  },
+  "places__title4": {
+    "ru": "Зимний Байкал",
+    "en": "Winter Baikal",
+  },
+  "places__title5": {
+    "ru": "Карелия",
+    "en": "Karelia",
+  },
+  "places__image1": {
+    "ru": "Куршская коса",
+    "en": "Curonian Spit",
+  },
+  "places__image2": {
+    "ru": "Кольский полуостров",
+    "en": "Kola Peninsula",
+  },
+  "places__image3": {
+    "ru": "лес Алтая",
+    "en": "Altai forest",
+  },
+  "places__image4": {
+    "ru": "замерзший байкал",
+    "en": "frozen Baikal",
+  },
+  "places__image5": {
+    "ru": "Карелия",
+    "en": "Karelia",
+  },
+  "places__paragraph1": {
+    "ru": "Здесь, посреди лесов и песчаных дюн, вы сможете увидеть два водных горизонта — спокойного Куршского залива с одной стороны и\n" +
+      "                подёрнутого рябью волн Балтийского моря с другой. Уникальная природная зона на краю российского анклава.",
+    "en": "Here, in the middle of forests and sand dunes, you can see two water horizons - the calm Curonian Lagoon on one side and the rippling waves of the Baltic Sea on the other. A unique natural area on the edge of the Russian enclave.",
+  },
+  "places__paragraph2": {
+    "ru": "На этом Калининградская область не заканчивается. Для путешественника и исследователя там же по соседству — самая западная\n" +
+      "                точка России, Балтийская коса, — и немецкое наследие россыпи небольших приморских городов. Атмосфера здешних мест исключает суету, окуная в спокойствие \n" +
+      "                природы и запах стального, прохладного моря.",
+    "en": "The Kaliningrad region does not end there. For the explorer traveler, there is the westernmost point of Russia, the Baltic Spit, and the German heritage of a scattering of small seaside towns in the same neighborhood. The atmosphere of these places eliminates the fuss, dipping into the tranquility of nature and the smell of steel, cool sea.",
+  },
+  "places__paragraph3": {
+    "ru": "Почти весь полуостров находится за Полярным кругом. Саамская тундра, от которой на юг — тайга, а на север — Ледовитый океан,\n" +
+      "                прикидывающийся Баренцевым морем.",
+    "en": "Almost the entire peninsula is located beyond the Arctic Circle. The Saami tundra, from which to the south is the taiga, and to the north is the Arctic Ocean, pretending to be the Barents Sea.",
+  },
+  "places__paragraph4": {
+    "ru": "Возможно, вы смотрели Звягинцева и даже слышали историю арктического фестиваля в Териберке. Возможно, слово «Хибины» не\n" +
+      "                осталось под снегом школьных воспоминаний об уроках географии. Возможно, вы не интересовались пронизывающей земную кору сверхглубокой скважиной, а от \n" +
+      "                апатитов вас давно накрывает апатия. Но ваша мечта увидеть северное сияние начинает сбываться с билетом в Мурманск.",
+    "en": "Perhaps you watched Zvyagintsev and even heard the story of the Arctic festival in Teriberka. Perhaps the word \"Khibiny\" did not remain under the snow of school memories of geography lessons. Perhaps you were not interested in the ultra-deep well penetrating the earth's crust, but apathy has long covered you from apatites. But your dream of seeing the northern lights comes true with a ticket to Murmansk.",
+  },
+  "places__paragraph5": {
+    "ru": "Алтай — одно из красивейших мест в России. В первую очередь из-за гор: если ехать вдоль хребта, вы увидите склоны, усыпанные\n" +
+      "                соснами, горные реки и озёра. А если вы откроете в автомобиле окна, сможете познакомиться с невидимым чудом здешних мест — горным воздухом.",
+    "en": "Altai is one of the most beautiful places in Russia. First of all, because of the mountains: if you drive along the ridge, you will see slopes strewn with pine trees, mountain rivers and lakes. And if you open the windows in the car, you can get acquainted with the invisible miracle of these places - mountain air.v",
+  },
+  "places__paragraph6": {
+    "ru": "Климат на Алтае умеренный, поэтому ехать сюда лучше всего летом. Так вы увидите всё разнообразие местной флоры и фауны. По\n" +
+      "                лесам Алтая бродят лоси, над хребтами летают орлы, а на равнинах пасутся косули. И знаменитые манулы — тоже обитатели Алтайского края.",
+    "en": "The climate in Altai is temperate, so it is best to go here in summer. So you will see all the variety of local flora and fauna. Moose roam the forests of Altai, eagles fly over the ridges, and roe deer graze on the plains. And the famous manuls are also inhabitants of the Altai Territory.",
+  },
+  "places__paragraph7": {
+    "ru": "Всем известен Байкал как крупнейшее озеро в мире. Многие также знают, что это самый большой источник пресной воды и одно из\n" +
+      "                красивейших мест в России.",
+    "en": "Everyone knows Baikal as the largest lake in the world. Many also know that this is the largest source of fresh water and one of the most beautiful places in Russia.",
+  },
+  "places__paragraph8": {
+    "ru": "Конечно, это всё так. Но Байкал ещё идеальное место для соревнований по скийорингу. Это такой вид спорта, когда лыжник\n" +
+      "                привязывает себя к мотоциклу, и тандем старается развить как можно бóльшую скорость на льду. В марте 2019 года на фестивале «Байкальская миля» был \n" +
+      "                поставлен мировой рекорд — 197.011 км/ч.",
+    "en": "Of course, it's all true. But Baikal is still an ideal place for skiing competitions. This is a sport where a skier ties himself to a motorcycle and the tandem tries to develop as much speed as possible on the ice. In March 2019, a world record was set at the Baikal Mile festival - 197.011 km/h.",
+  },
+  "places__paragraph9": {
+    "ru": "Сибирь заканчивается не на Урале, а в Карелии: образующая тайгу сибирская лиственница не растёт западнее Водлозера. Зато здесь\n" +
+      "                она вымахивает на 30 метров — леса карельских национальных парков из-за непроходимых болот никогда не знали топора. Некоторым соснам уже больше чем \n" +
+      "                полтысячелетия. Прикоснитесь к живому существу, видевшему солнце раньше, чем увидал его Иван Грозный. В девственном лесу на сотню километров не \n" +
+      "                встретишь тропы. А на редких тропинках деревья в паре метров от земли помечены медвежьими когтями. Чтобы все знали, кто тут хозяин.",
+    "en": "Siberia ends not in the Urals, but in Karelia: the Siberian larch that forms the taiga does not grow west of the Vodlozero. But here it swings up to 30 meters - the forests of the Karelian national parks, due to impenetrable swamps, have never known an ax. Some pines are more than half a millennium old. Touch a living creature that saw the sun before Ivan the Terrible saw it. In the virgin forest for a hundred kilometers you will not find a path. And on rare paths, trees a couple of meters from the ground are marked with bear claws. Let everyone know who's boss.",
+  },
+  "cover__title": {
+    "ru": "До Байкала «на&nbsp;собаках»",
+    "en": "To Baikal \"on&nbsp;dogs\"",
+  },
+  "cover__subtitle": {
+    "ru": "По мотивам учебной темы о Транссибе — путешествие от столицы до Байкала на электричках",
+    "en": "Based on the educational topic about the Trans-Siberian Railway - a journey from the capital to Baikal by electric trains",
+  },
+  "footer__link1": {
+    "ru": "Карты",
+    "en": "Maps",
+  },
+  "footer__link2": {
+    "ru": "Погода",
+    "en": "Weather",
+  },
+  "footer__link3": {
+    "ru": "Расписание",
+    "en": "Schedule",
+  },
+  "footer__link4": {
+    "ru": "Календарь",
+    "en": "Calendar",
+  },
+  "footer__link5": {
+    "ru": "Путешествие",
+    "en": "Journey",
+  },
+  "footer__copyright": {
+    "ru": "&copy; 2022. Ирина Осипова",
+    "en": "&copy; 2022. Irina Osipova"
   }
 }
--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const select = document.querySelector('select');
 const allLang = ["ru", "en"];
+// const arrPhotoGridItem = document.querySelectorAll('.photo-grid__item');
+// const arrAltPhotoGridItem = [];
+//
+// arrPhotoGridItem.forEach((item) => arrAltPhotoGridItem.push(item.alt));
 
 select.addEventListener('change', changeURLLanguage);
 
@@ -19,11 +23,12 @@ function changeLanguage() {
 
   select.value = hash;
 
-  document.querySelector("html").lang = hash;
   document.querySelector("title").innerHTML = langArr["lead__title"][hash];
+  document.querySelector("html").lang = hash;
   document.querySelector("meta[name='description']").content = langArr["metaDescription"][hash];
   document.querySelector("meta[name='keywords']").content = langArr["metaKeywords"][hash];
   document.querySelector("meta[name='author']").content = langArr["metaAuthor"][hash];
+  document.querySelector(".header__logo").src = "images/header-logo-en.svg";
 
   for (let key in langArr) {
     let element = document.querySelector(`.${key}`);
@@ -32,6 +37,17 @@ function changeLanguage() {
       element.innerHTML = translate;
     }
   }
+
+  for (let key in langArr) {
+    let element = document.querySelector(`.${key}`);
+    let translate = langArr[key][hash];
+    if (element && translate) {
+      element.alt = translate;
+    }
+  }
+
+
+
 }
 
 changeLanguage();
